b1e4d944b966870b48e327f3baaddd98cfdedfdd
caitxj <654934156@qq.com>
2017-08-19 08:23:46 +0800

    js  update.....

---
+++ javascript/jquery/basic/jqTable.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * jquery    table
+ *
+ * Created by caitxj on 17-8-18.
+ */
+
+var jqTable = {
+    /**
+     * 事件初始化操作
+     */
+    init: function () {
+        $("#add").click(function () {
+
+        })
+        $("#query").click(function () {
+        })
+        $("#keyWord").keydown(function (event) {
+            if (event.keyCode == 13) {
+            }
+        })
+    }
+    ,
+    add: function () {
+
+    }
+    ,
+    delete: function () {
+
+    }
+    ,
+    editor: function () {
+
+    }
+    ,
+    cancel: function () {
+
+    }
+    ,
+    query: function () {
+
+    }
+}
